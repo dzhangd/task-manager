a590e74832c0d21cbe28e931b757d22838b73b8e
ksyeo1010 <ksyeo1996@gmail.com>
2016-03-26 19:00:51 -0700

    added getProjects method

--- src/database/Project.java
+++ src/database/Project.java
@@ -3,22 +3,43 @@ package database;
 
 import connection.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import static connection.DatabaseConnection.getConnection;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by Kwangsoo on 2016-03-19.
  */
 public class Project {
-    Connection con;
+    private Connection con;
+    public String[] project;
 
     public Project() {
     }
 
+    public ArrayList getProjects() {
+        con = DatabaseConnection.getConnection();
+        ArrayList<String[]> projects = new ArrayList<String[]>();
+        try {
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM Project");
+            while(rs.next()) {
+                project = new String[3];
+                setProject(rs.getString(1), rs.getString(2), rs.getString(3));
+                projects.add(project);
+            }
+            project = null;
+            return projects;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    public void setProject(String pid, String name, String description) {
+        project[0] = pid;
+        project[1] = name;
+        project[2] = description;
+    }
 
     public void addProject(int pid, String name, String description) {
         con = DatabaseConnection.getConnection();
