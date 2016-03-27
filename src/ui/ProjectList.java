3035cc520a08a9e6b1e1efde69bd8b61c2d92f57
ksyeo1010 <ksyeo1996@gmail.com>
2016-03-26 19:01:21 -0700

    show project list on GUI

--- src/ui/ProjectList.java
+++ src/ui/ProjectList.java
@@ -1,30 +1,46 @@
 package ui;
 
+import connection.DatabaseConnection;
+import database.Project;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.xml.crypto.Data;
 
 public class ProjectList extends JPanel {
+
+    Project project;
+    ArrayList<String> projectNames;
+    DefaultListModel listModel = new DefaultListModel();
 
 	public ProjectList()
 	{
 		this.setBackground(Color.GREEN);
 		
 		this.setLayout(new BorderLayout());
-		
-		DefaultListModel listModel = new DefaultListModel();
-        listModel.addElement("304 project");
-        listModel.addElement("Herp project");
-        listModel.addElement("Derp Project");
-		
+
+        project = new Project();
+        projectNames = new ArrayList<String>();
+        setProjectsNames();
+        for (int i=0; i<projectNames.size();i++) {
+            listModel.addElement(projectNames.get(i));
+        }
+
+        
 		JList projectJList = new JList(listModel);
 		projectJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		projectJList.setSelectedIndex(0);
@@ -35,5 +51,12 @@ public class ProjectList extends JPanel {
 		
 		
 	}
-	
+
+    public void setProjectsNames() {
+        ArrayList<String[]> projects = project.getProjects();
+        for (int i=0; i<projects.size(); i++) {
+            projectNames.add(projects.get(i)[1]);
+        }
+    }
+
 }
