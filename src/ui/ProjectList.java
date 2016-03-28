3bd176155e5cef16f2f4e3b5809256a3108649d0
Joseph Choi <robocup30@hotmail.com>
2016-03-27 19:10:57 -0700

    Added basic listener to project and task list

--- src/ui/ProjectList.java
+++ src/ui/ProjectList.java
@@ -11,6 +11,8 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class ProjectList extends JPanel {
 
@@ -24,15 +26,31 @@ public class ProjectList extends JPanel {
         listModel.addElement("304 project");
         listModel.addElement("Herp project");
         listModel.addElement("Derp Project");
+        listModel.addElement("Derpa Project");
+        listModel.addElement("Derpb Project");
 		
 		JList projectJList = new JList(listModel);
+	    ListSelectionModel listSelectionModel = projectJList.getSelectionModel();
+	    listSelectionModel.addListSelectionListener(new ProjectListListener());
 		projectJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		projectJList.setSelectedIndex(0);
         
 		JScrollPane projectScrollPane = new JScrollPane(projectJList);
 		this.add(projectScrollPane, BorderLayout.CENTER);
-		
-		
+	}
+	
+	class ProjectListListener implements ListSelectionListener
+	{
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+	        int firstIndex = e.getFirstIndex();
+	        int lastIndex = e.getLastIndex();
+	        int selectedIndex = lsm.getLeadSelectionIndex();
+	        System.out.println("Project selected is: " + selectedIndex);
+			
+		}
 		
 	}
 	
--- src/ui/TaskList.java
+++ src/ui/TaskList.java
@@ -9,6 +9,10 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import ui.ProjectList.ProjectListListener;
 
 public class TaskList extends JPanel {
 
@@ -22,12 +26,30 @@ public class TaskList extends JPanel {
         listModel.addElement("Make Database");
         listModel.addElement("Make GUI");
         listModel.addElement("Random Task");
+        listModel.addElement("Random Task2");
+        listModel.addElement("Random Task3");
 		
 		JList taskJList = new JList(listModel);
+	    ListSelectionModel listSelectionModel = taskJList.getSelectionModel();
+	    listSelectionModel.addListSelectionListener(new TaskListListener());
 		taskJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		taskJList.setSelectedIndex(0);
         
 		JScrollPane taskScrollPane = new JScrollPane(taskJList);
 		this.add(taskScrollPane, BorderLayout.CENTER);
+	}
+	
+	class TaskListListener implements ListSelectionListener
+	{
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+	        int firstIndex = e.getFirstIndex();
+	        int lastIndex = e.getLastIndex();
+	        int selectedIndex = lsm.getLeadSelectionIndex();
+	        System.out.println("Task selected is: " + selectedIndex);			
+		}
+		
 	}
 }
