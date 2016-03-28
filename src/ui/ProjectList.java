a3008687769acb5c698da77e51f35aeb4798f2a4
ksyeo1010 <ksyeo1996@gmail.com>
2016-03-28 05:46:12 -0700

    connected projectlist and tasklist with database

--- src/ui/ProjectList.java
+++ src/ui/ProjectList.java
@@ -1,62 +1,176 @@
 package ui;
 
-import connection.DatabaseConnection;
 import database.Project;
+import database.Task;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
-import javax.xml.crypto.Data;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import static ui.Startup.taskListPanel;
 
 public class ProjectList extends JPanel {
 
-    Project project;
-    ArrayList<String> projectNames;
-    DefaultListModel listModel = new DefaultListModel();
+    JLabel projectDescription = new JLabel("Project Description");
+    Project project = new Project();
+    ArrayList<Object[]> projects = project.getProjects();
+    Task task = new Task();
+    ArrayList<Object[]> tasks = task.getTasks();
 
-	public ProjectList()
-	{
-		this.setBackground(Color.GREEN);
-		
-		this.setLayout(new BorderLayout());
+    public ProjectList()
+    {
+        GridBagConstraints gbc = new GridBagConstraints();
+        this.setBackground(Color.WHITE);
 
-        project = new Project();
-        projectNames = new ArrayList<String>();
-        setProjectsNames();
-        for (int i=0; i<projectNames.size();i++) {
-            listModel.addElement(projectNames.get(i));
-        }
+        this.setLayout(new GridBagLayout());
 
-        
-		JList projectJList = new JList(listModel);
-		projectJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		projectJList.setSelectedIndex(0);
-        
-		JScrollPane projectScrollPane = new JScrollPane(projectJList);
-		this.add(projectScrollPane, BorderLayout.CENTER);
-		
-		
-		
-	}
-
-    public void setProjectsNames() {
-        ArrayList<String[]> projects = project.getProjects();
+        DefaultListModel listModel = new DefaultListModel();
         for (int i=0; i<projects.size(); i++) {
-            projectNames.add(projects.get(i)[1]);
+            String temp = (String) projects.get(i)[1];
+            listModel.addElement(temp.trim());
         }
+
+        JList projectJList = new JList(listModel);
+        ListSelectionModel listSelectionModel = projectJList.getSelectionModel();
+        listSelectionModel.addListSelectionListener(new ProjectListListener());
+        projectJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        projectJList.setSelectedIndex(0);
+
+        JScrollPane projectScrollPane = new JScrollPane(projectJList);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 0.73;
+        this.add(projectScrollPane, gbc);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 1;
+        gbc.weighty = 0.10;
+        this.add(projectDescription, gbc);
+
+        JButton renameProjectButton = new JButton("Rename Project");
+        JButton addProjectButton = new JButton("Create Project");
+        JButton removeProjectButton = new JButton("Delete Project");
+
+        renameProjectButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("Rename Project Button Clicked");
+                JTextField newNameField = new JTextField();
+                Object[] message = {
+                        "New name:", newNameField,
+                };
+                String s = (String)JOptionPane.showInputDialog(null, "Type new name", "Rename", JOptionPane.PLAIN_MESSAGE, null, null, null);
+                System.out.println("Rename project to " + s);
+            }
+        });
+        addProjectButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("Add Project Button Clicked");
+                JTextField projectName = new JTextField();
+                JTextField projectDescription = new JTextField();
+                Object[] message = {
+                        "Project name:", projectName,
+                        "Project Description:", projectDescription
+                };
+                int addProjectSelection = JOptionPane.showConfirmDialog(null, message, "Add Project", JOptionPane.OK_CANCEL_OPTION, JOptionPane.DEFAULT_OPTION);
+                if(addProjectSelection != 0)
+                {
+                    return;
+                }
+                System.out.println("Project name is " + projectName.getText() + " Project description is " + projectDescription.getText());
+            }
+        });
+        removeProjectButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("Remove Project Button Clicked");
+                Object[] options = {"Yes",
+                        "No",
+                };
+                int optionChosen = JOptionPane.showOptionDialog(null,
+                        "Are you sure you want to remove this project",
+                        "Remove Project",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        options[1]);
+                System.out.println("Remove project option " + optionChosen);
+            }
+        });
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 1;
+        gbc.weighty = 0.05;
+        this.add(renameProjectButton, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.weightx = 1;
+        gbc.weighty = 0.05;
+        this.add(addProjectButton, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.weightx = 1;
+        gbc.weighty = 0.05;
+        this.add(removeProjectButton, gbc);
+    }
+
+    int selectedIndex = 0;
+
+    public class ProjectListListener implements ListSelectionListener
+    {
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+            int firstIndex = e.getFirstIndex();
+            int lastIndex = e.getLastIndex();
+            selectedIndex = lsm.getLeadSelectionIndex();
+            System.out.println("Project selected is: " + selectedIndex);
+
+            String description = (String) projects.get(selectedIndex)[2];
+            projectDescription.setText(description.trim());
+
+            taskListPanel = new TaskList();
+            taskListPanel.listModel.clear();
+            int pid = (Integer) projects.get(selectedIndex)[0];
+            for (int i=0; i<tasks.size(); i++) {
+                int pid2 = (Integer) tasks.get(i)[9];
+                if (pid == pid2) {
+                    String title = (String) tasks.get(i)[1];
+                    taskListPanel.listModel.addElement(title.trim());
+                }
+            }
+        }
+
     }
 
 }
--- src/ui/TaskList.java
+++ src/ui/TaskList.java
@@ -3,31 +3,154 @@ package ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import database.Project;
+import database.Task;
+import ui.ProjectList.ProjectListListener;
 
 public class TaskList extends JPanel {
 
+	static DefaultListModel listModel = new DefaultListModel();
+
 	public TaskList()
 	{
+
+		GridBagConstraints gbc = new GridBagConstraints();
 		this.setBackground(Color.CYAN);
-		
-		this.setLayout(new BorderLayout());
-		
-		DefaultListModel listModel = new DefaultListModel();
-        listModel.addElement("Make Database");
-        listModel.addElement("Make GUI");
-        listModel.addElement("Random Task");
-		
+
+		this.setLayout(new GridBagLayout());
+
 		JList taskJList = new JList(listModel);
+		ListSelectionModel listSelectionModel = taskJList.getSelectionModel();
+		listSelectionModel.addListSelectionListener(new TaskListListener());
 		taskJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		taskJList.setSelectedIndex(0);
-        
+
 		JScrollPane taskScrollPane = new JScrollPane(taskJList);
-		this.add(taskScrollPane, BorderLayout.CENTER);
+
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 0.85;
+		this.add(taskScrollPane, gbc);
+
+		JButton renameTaskButton = new JButton("Rename Task");
+		JButton addTaskButton = new JButton("Add Task");
+		JButton removeTaskButton = new JButton("Remove Task");
+
+		renameTaskButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Rename Task Button Clicked");
+				JTextField newNameField = new JTextField();
+				Object[] message = {
+						"New name:", newNameField,
+				};
+				String s = (String)JOptionPane.showInputDialog(null, "Type new name", "Rename", JOptionPane.PLAIN_MESSAGE, null, null, null);
+				System.out.println("Rename task to " + s);
+			}
+		});
+		addTaskButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Add Task Button Clicked");
+				JTextField taskName = new JTextField();
+				JTextField taskDescription = new JTextField();
+				Object[] type = {"Bug", "Feature"};
+				JComboBox typeCombo = new JComboBox(type);
+				Object[] priority = {"1", "2", "3", "4", "5"};
+				JComboBox priorityCombo = new JComboBox(priority);
+				Object[] message = {
+						"Task name:", taskName,
+						"Task Description:", taskDescription,
+						"Type:", typeCombo,
+						"Priority:", priorityCombo
+				};
+				int addTaskSelection = JOptionPane.showConfirmDialog(null, message, "Add Task", JOptionPane.OK_CANCEL_OPTION, JOptionPane.DEFAULT_OPTION);
+				if(addTaskSelection != 0)
+				{
+					return;
+				}
+				System.out.println("Task name is " + taskName.getText() + " Task description is " + taskDescription.getText() + " type index is " + typeCombo.getSelectedIndex() + " Priority index is " + priorityCombo.getSelectedIndex());
+			}
+		});
+		removeTaskButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Remove Task Button Clicked");
+				Object[] options = {"Yes",
+						"No",
+				};
+				int optionChosen = JOptionPane.showOptionDialog(null,
+						"Are you sure you want to remove this task",
+						"Remove Task",
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,
+						options,
+						options[1]);
+				System.out.println("Remove task option " + optionChosen);
+			}
+		});
+
+
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0.05;
+		this.add(renameTaskButton, gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weightx = 1;
+		gbc.weighty = 0.05;
+		this.add(addTaskButton, gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.weightx = 1;
+		gbc.weighty = 0.05;
+		this.add(removeTaskButton, gbc);
 	}
+
+	class TaskListListener implements ListSelectionListener
+	{
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+			int firstIndex = e.getFirstIndex();
+			int lastIndex = e.getLastIndex();
+			int selectedIndex = lsm.getLeadSelectionIndex();
+			System.out.println("Task selected is: " + selectedIndex);
+		}
+
+	}
+
 }
