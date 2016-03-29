31e6ef9aa4d6b2d60d23e16c6242ccb07d4dd333
ksyeo1010 <ksyeo1996@gmail.com>
2016-03-28 22:01:08 -0700

    new methods and changes to update gui

--- src/database/Project.java
+++ src/database/Project.java
@@ -12,13 +12,14 @@ import java.util.ArrayList;
 public class Project {
     private Connection con;
     public Object[] project;
+    ArrayList<Object[]> projects;
 
     public Project() {
     }
 
     public ArrayList getProjects() {
         con = DatabaseConnection.getConnection();
-        ArrayList<Object[]> projects = new ArrayList<Object[]>();
+        projects = new ArrayList<Object[]>();
         try {
             Statement s = con.createStatement();
             ResultSet rs = s.executeQuery("SELECT * FROM Project");
@@ -42,6 +43,38 @@ public class Project {
         project[2] = description;
     }
 
+    public void renameProject(int pid, String name) {
+        con = DatabaseConnection.getConnection();
+        try {
+            PreparedStatement ps = con.prepareStatement("UPDATE Project SET name = ? WHERE pid = ?");
+            ps.setString(1, name);
+            ps.setInt(2,pid);
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getMaxPid(){
+        con = DatabaseConnection.getConnection();
+        int maxPid = 0;
+        try {
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery("SELECT MAX(pid) FROM Project");
+            if (rs.next()) {
+                maxPid = rs.getInt(1);
+            }
+
+            s.close();
+            return maxPid;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
     public void addProject(int pid, String name, String description) {
         con = DatabaseConnection.getConnection();
         try {
@@ -55,6 +88,11 @@ public class Project {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        project = new Object[3];
+        project[0] = pid;
+        project[1] = name;
+        project[2] = description;
+        projects.add(project);
     }
 
     public void deleteProject(int pid) {
@@ -67,6 +105,12 @@ public class Project {
             ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        for (int i=0; i<projects.size();i++) {
+            if ((Integer) projects.get(i)[0] == pid) {
+                projects.remove(i);
+                return;
+            }
         }
     }
 }
